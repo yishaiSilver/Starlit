6c74e99a9f5303040e2d2393ec5d148bba810444
Yishai Silver <yishaisilver8@gmail.com>
2020-02-09 19:27:30 -0800

    Fixed stars.

--- Starlit/Assets/Scripts/StarsBackground.cs
+++ Starlit/Assets/Scripts/StarsBackground.cs
@@ -8,17 +8,23 @@ using UnityEngine;
 
 public class StarsBackground : MonoBehaviour
 {
+    SpriteRenderer mr;
+
+    Material mat;
 
     public float speedDenominator;
+
+    private void Start()
+    {
+        mr = GetComponent<SpriteRenderer>();
+        mat = mr.material;
+
+        mat.mainTexture.wrapMode = TextureWrapMode.Repeat;
+    }
 
     // Update is called once per frame
     void Update()
     {
-
-        SpriteRenderer mr = GetComponent<SpriteRenderer>();
-
-        Material mat = mr.material;
-
         Vector2 offset = mat.mainTextureOffset;
 
         offset.x = transform.position.x / (transform.localScale.x * speedDenominator);
