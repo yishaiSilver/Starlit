e09d74804c5737d826549c3aedd4c2f8211d4c5c
Yishai Silver <yishaisilver8@gmail.com>
2020-02-09 17:18:03 -0800

    Started adding scripts, gitignore reflects InGameCodeEditor and RoslynCSharp

---
+++ Starlit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public Ship personalShip;
+
+    private GameObject canvas;
+    private HUDManager hudManager;
+
+    private bool controlsEnabled = true;
+
+    public void Start()
+    {
+        personalShip.setPlayerController(this);
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        hudManager = canvas.GetComponent<HUDManager>();
+    }
+
+    public void FixedUpdate()
+    {
+        transform.position = personalShip.transform.position;
+        if (controlsEnabled)
+        {
+            if (!personalShip.Manual())
+            {
+                personalShip.Automatic();
+            }
+            else
+            {
+                personalShip.StopAutomatic();
+                SetTarget("");
+            }
+        }
+        else
+        {
+            personalShip.MoveTowards();
+        }
+    }
+
+    public void LateUpdate()
+    {
+    }
+
+    public void notifyOfTakeoff()
+    {
+        hudManager.Reset();
+    }
+
+    public void notifyOfLand(GameObject ship, GameObject target)
+    {
+        hudManager.Land(ship, target);
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        GameObject targetBox = canvas.transform.Find("HUD").Find("Target").gameObject;
+        Text text = targetBox.GetComponent<Text>();
+        text.text = target.name.ToUpper();
+    }
+
+    public void SetTarget(string str)
+    {
+        GameObject targetBox = canvas.transform.Find("HUD").Find("Target").gameObject;
+        Text text = targetBox.GetComponent<Text>();
+        text.text = str.ToUpper();
+    }
+
+    public void disableControls()
+    {
+        controlsEnabled = false;
+    }
+
+    public void enableControls()
+    {
+        controlsEnabled = true;
+    }
+}
---
+++ Starlit/Assets/Scripts/StarsBackground.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Class used to create parallax effect of stars
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsBackground : MonoBehaviour
+{
+
+    public float speedDenominator;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        SpriteRenderer mr = GetComponent<SpriteRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x = transform.position.x / (transform.localScale.x * speedDenominator);
+        offset.y = transform.position.y / (transform.localScale.y * speedDenominator);
+
+        mat.mainTextureOffset = offset;
+
+    }
+}
